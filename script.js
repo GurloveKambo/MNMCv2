71f974db2b318d147038ec269f6f81f73d908543
GurloveKambo <integrityneeds@gmail.com>
2026-05-10 14:10:18 +1000

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('.hamb')?.addEventListener('click',()=>document.querySelector('.links').classList.toggle('open'));
-const y=document.getElementById('year'); if(y) y.textContent=new Date().getFullYear();
